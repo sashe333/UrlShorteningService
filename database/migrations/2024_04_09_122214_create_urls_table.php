ad16018bf9105652816d46b685402061e9f81fec
sashe trajkovski <sashetrajkovskiii@gmail.com>
2024-04-09 14:37:09 +0200

    Added migration, model, controller, factory and seeder for urls table. Migration tested with data

---
+++ database/migrations/2024_04_09_122214_create_urls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('url_short')->nullable();
+            $table->unsignedInteger('short_url_access_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urls');
+    }
+};
